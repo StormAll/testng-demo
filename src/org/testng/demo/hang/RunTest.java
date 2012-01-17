9c9ce0bfd8e9ace0d3cc873a45a30296562d202c
Storm Qi <storm.cen.qi@gmail.com>
2012-01-17 14:10:36 +0800

    first commit

---
+++ src/org/testng/demo/hang/RunTest.java
@@ -0,0 +1,30 @@
+package org.testng.demo.hang;
+
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class RunTest {
+
+	@Parameters("test001_name")
+	@Test
+	public void test001(String name) {
+		System.out.println("this is a test001 method. name: " + name);
+	}
+	
+	@Parameters({"test002_num1", "test002_num2"})
+	@Test
+	public void test002(int num1, int num2) {
+		System.out.println("this is a test002 method.");
+		System.out.println(num1 + " + " + num2 + " = " + (num1 + num2));
+	}
+	
+	@Test
+	public void test003() {
+		System.out.println("this is a test003 method.");
+	}
+	
+	@Test
+	public void test004() {
+		System.out.println("this is a test004 method.");
+	}
+}
